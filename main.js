db747552569ba76b90f54c4f3fd73b2205fac622
Rajesh25B <basvoju.rajesh@gmail.com>
2023-07-01 17:31:28 +0530

    showing the correct tab panel when we click on a new tab

--- main.js
+++ main.js
@@ -11,3 +11,27 @@ tabButtons.forEach((tab, idx) => {
         tabPanels[idx].setAttribute("hidden", "");
     }
 });
+
+tabsContainer.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const clickedTab = event.target.closest('a'); // To know which tab user was clicked
+    if (!clickedTab) {
+        return;
+    } // returns null if click happens outside of anchor tags, prevent null value showing.
+
+    switchTab(clickedTab)
+});
+
+function switchTab(newTab) {
+    // get active panel upon tab selection
+    const activePanelId = newTab.getAttribute('href'); // returns href of each tab
+    const activePanel = tabsContainer.querySelector(activePanelId);
+
+    // loop through each panel and make them hidden
+    tabPanels.forEach((panel) => {
+        panel.setAttribute("hidden", true);
+    });
+    // upon selection of a single panel, remove href attribute of hidden on that panel
+    activePanel.removeAttribute("hidden")
+}
